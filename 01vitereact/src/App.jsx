3099808439a59c849e598968861c431f4786aefa
kapil-maan <kapilmaan0123@gmail.com>
2024-07-19 10:28:29 +0530

    create first component

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+function App() {
+   
+
+  return (
+    <>
+      <Chai />
+      <h1>ready</h1>
+      <p>take a sip</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>my chai</h3>
+    )
+}
+
+export default Chai 
